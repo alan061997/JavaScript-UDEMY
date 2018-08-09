dcefa6664a5baa8a3efa7f06919ec2d7add2374b
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-09 10:35:55 -0500

    Section 5 - Lecture 17

--- hola-mundo/hola-mundo.js
+++ hola-mundo/hola-mundo.js
@@ -1,3 +1,5 @@
-alert("Hola Mundo desde un archivo de JavaScript")
+alert("Hola Mundo desde un archivo de JavaScript");
 alert("¡Bienvenido al curso!");
-document.write("Este archivo es externo al html")
+document.write("Este archivo es externo al html");
+console.log("Mensaje en la consola");
+console.log(2 + 2);
