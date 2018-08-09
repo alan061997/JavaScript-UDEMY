d6a2b23e07490cd8a9f3966c00969fad2b329e29
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-09 11:13:54 -0500

    Section 6 - Lecture 18

--- hola-mundo/js/01-hola-mundo.js
+++ hola-mundo/js/01-hola-mundo.js
@@ -1,5 +1,8 @@
 alert("Hola Mundo desde un archivo de JavaScript");
 alert("¡Bienvenido al curso!");
 document.write("Este archivo es externo al html");
+
+/*
 console.log("Mensaje en la consola");
 console.log(2 + 2);
+*/
---
+++ hola-mundo/js/02-variables.js
@@ -0,0 +1,13 @@
+// VARIABLES
+// Una variable es un contenedor de informacion
+
+var pais = "Mexico";
+var continente = "America";
+var antiguedad = 2019;
+var pais_y_continente = pais + ' ' + continente;
+
+pais = "España";
+continente = "Europa";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
