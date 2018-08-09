88d06d83376d263435bbcfd938adaf49333003be
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-09 12:35:53 -0500

    Section 8 - Lecture 25

--- hola-mundo/js/06-condicionales.js
+++ hola-mundo/js/06-condicionales.js
@@ -30,6 +30,7 @@ var nombre = "Alberto Alan Zul Rabasa";
  */
 
 if (edad >= 18) {
+    // Es mayor de edad
     console.log(nombre + " tiene " + edad + " años, es mayor de edad.");
 
     if (edad <= 33) {
@@ -40,5 +41,34 @@ if (edad >= 18) {
         console.log("Eres anciano.");
     }
 } else {
+    // Es menor de edad
     console.log(nombre + " tiene " + edad + " años, es menor de edad.");
+}
+
+/*
+// Operadores Logicos
+AND: &&
+OR: ||
+NEGACIÓN: !
+ */
+
+var year = 2018;
+
+// Negación
+if (year != 2016) {
+    console.log("El año no es 2016, es el año " + year + ".");
+}
+
+// AND
+if (year >= 2000 && year <= 2020) {
+    console.log("Estamos en la era actual.");
+} else {
+    console.log("Estamos en la era post moderna.");
+}
+
+// OR
+if (year == 2008 || (year >= 2018 && year == 2028)) {
+    console.log("El año acaba en 8.");
+} else {
+    console.log("AÑO NO REGISTRADO.");
 }
