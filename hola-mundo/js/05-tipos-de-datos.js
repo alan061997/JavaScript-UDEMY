a0e02175db8f3803b5c9ce6321397fc52c7dd8da
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-09 11:48:09 -0500

    Section 7 - Lecture 22

---
+++ hola-mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("Resultado de la operacion: " + operacion);
+
+// Tipo de Datos
+var numeroEntero = 10;
+var cadenaTexto = 'Hola "que" tal';
+var verdaderoOfalso = false;
+
+var numeroFalso = "666";
+
+
+console.log(String(numeroEntero) + 4);
