206750855133df561ea66f1e232233bcb1271966
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-10 10:02:53 -0500

    Section 9 - Lecture 29

--- hola-mundo/js/09-while.js
+++ hola-mundo/js/09-while.js
@@ -2,7 +2,19 @@
 
 var year = 2018;
 
+// Bucle while
+
 while (year !== 1991) {
     console.log("Estamos en el año: " + year);
     year--;
 }
+
+
+// Bucle Do While
+
+var years = 30;
+
+do {
+    alert("SOLO CUANDO SEA DIFERENTE A 20.");
+    years--;
+} while (year > 25);
