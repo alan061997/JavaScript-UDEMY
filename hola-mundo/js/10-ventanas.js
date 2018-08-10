014e1bf5de3074bdb633ebf6b2af2831cc47c1ab
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-10 10:10:22 -0500

    Section 10 - Lecture 31

---
+++ hola-mundo/js/10-ventanas.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// ALERTAS
+alert("Esta es mi primera alerta");
+alert("Esta es mi segunda alerta");
+
+// CONFIRMACION
+var resultado1 = confirm("¿Seguro que quieres continuar?");
+console.log(resultado1);
+
+// INGRESO DE DATOS
+var resultado2 = prompt("¿Que edad tienes?", 21);
+console.log(resultado2);
