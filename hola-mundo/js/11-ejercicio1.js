9f4f594514928e196f9f590bc96f1b361e2fd431
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-10 10:53:31 -0500

    Section 11 - Lecture 33

--- hola-mundo/js/11-ejercicio1.js
+++ hola-mundo/js/11-ejercicio1.js
@@ -1,23 +1,28 @@
 "use strict";
 
 /*
-Programa que pida dos valores y determine cual es el mayor,  el menor, o si estos son iguales.
+EJERCICIO 1:
+    Programa que pida dos valores y determine cual es el mayor,  el menor, o si estos son iguales.
 */
 
-var numero1 = parseInt(prompt("Introduce el primer numero:", 0));
-var numero2 = parseInt(prompt("Introduce el primer numero:", 0));
 
-console.log(numero1, numero2);
+do {
+    var numero1 = parseInt(prompt("Introduce el primer numero:", 0));
+    var numero2 = parseInt(prompt("Introduce el segundo numero:", 0));
+    console.log(numero1, numero2);
+} while (isNaN(numero1) || isNan(numero2));
 
 if (numero1 !== numero2) {
     if (numero1 < numero2) {
         console.log("El primer numero es menor que el segundo numero");
         alert("El primer numero es menor que el segundo numero");
-    } else {
+    } else if (numero1 > numero2) {
         console.log("El primer numero es mayor que el segundo numero");
         alert("El primer numero es mayor que el segundo numero");
+    } else {
+        alert("Introduce valores numericos correctos.")
     }
-} else {
+} else if (numero1 === numero2) {
     console.log("Los dos numeros son iguales.");
     alert("Los dos numeros son iguales.");
 }
