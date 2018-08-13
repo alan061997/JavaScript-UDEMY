88c3f73b7dbd22320b51621f731e297a9e0ac279
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-13 09:50:15 -0500

    Section 11 - Lecture 35

---
+++ hola-mundo/js/13-ejercicio3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+Hacer un programa que muestre los numeros entre dos numeros mintroducidos por el usuario
+ */
+
+var num1 = parseInt(prompt("Primer numero:", 0));
+var num2 = parseInt(prompt("Segundo numero:", 0));
+
+
+document.write("<h1>Del " + num1 + "al " + num2 + "estan los numeros:</h1>");
+for (var i = num1; i < num2; i++) {
+    document.write("[" + i + "]");
+}
