e6ab61d7b3cffc52a8096111e80e805a3c3079c0
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-14 11:41:42 -0500

    Section 12 - Lecture 42

--- hola-mundo/js/Section 12/19-funciones.js
+++ hola-mundo/js/Section 12/19-funciones.js
@@ -2,12 +2,22 @@
 
 // Funciones
 
-function calculadora() {
-    console.log("Hola soy una calculadora");
-    console.log("Si soy yo");
-    return "Hola soy una calculadora";
+function calculadora(numero1, numero2) {
+    var resultado = "La suma de " + numero1 + " + " + numero2 + " es: " + (numero1 + numero2) + "\n" +
+        "La resta de " + numero1 + " - " + numero2 + " es: " + (numero1 - numero2) + "\n" +
+        "La multiplicacion de " + numero1 + " * " + numero2 + " es: " + (numero1 * numero2) + "\n" +
+        "La division de " + numero1 + " / " + numero2 + " es: " + (numero1 / numero2) + "\n";
+
+
+    console.log(resultado);
+    console.log("\n*********************************\n\n");
 }
 
-var resultado = calculadora();
+console.log("*********************************");
+console.log("********** CALCULADORA **********");
+console.log("*********************************\n\n");
 
-console.log(resultado);
+for (var i = 0; i <= 10; i++) {
+    console.log("[" + i + "]");
+    calculadora(i, 10);
+}
