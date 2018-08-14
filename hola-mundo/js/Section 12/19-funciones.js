147019713702f5557b7807643b55a22fcf86bbcf
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-14 11:48:46 -0500

    Section 12 - Lecture 43

--- hola-mundo/js/Section 12/19-funciones.js
+++ hola-mundo/js/Section 12/19-funciones.js
@@ -2,22 +2,38 @@
 
 // Funciones
 
-function calculadora(numero1, numero2) {
+function calculadora(numero1, numero2, mostrar = false) {
     var resultado = "La suma de " + numero1 + " + " + numero2 + " es: " + (numero1 + numero2) + "\n" +
         "La resta de " + numero1 + " - " + numero2 + " es: " + (numero1 - numero2) + "\n" +
         "La multiplicacion de " + numero1 + " * " + numero2 + " es: " + (numero1 * numero2) + "\n" +
         "La division de " + numero1 + " / " + numero2 + " es: " + (numero1 / numero2) + "\n";
 
+    var resultadoWEB = "La suma de " + numero1 + " + " + numero2 + " es: " + (numero1 + numero2) + "<br/>" +
+        "La resta de " + numero1 + " - " + numero2 + " es: " + (numero1 - numero2) + "<br/>" +
+        "La multiplicacion de " + numero1 + " * " + numero2 + " es: " + (numero1 * numero2) + "<br/>" +
+        "La division de " + numero1 + " / " + numero2 + " es: " + (numero1 / numero2) + "<br/>";
 
-    console.log(resultado);
-    console.log("\n*********************************\n\n");
+    if (mostrar === false) {
+        console.log(resultado);
+        console.log("\n*********************************\n\n");
+    } else {
+        document.write("*********************************<br/>");
+        document.write("******** CALCULADORA *********<br/>");
+        document.write("*********************************<br/><br/>");
+        document.write(resultadoWEB);
+        document.write("<br/>*********************************<br/><br/>");
+    }
 }
 
 console.log("*********************************");
 console.log("********** CALCULADORA **********");
 console.log("*********************************\n\n");
 
-for (var i = 0; i <= 10; i++) {
+/*for (var i = 0; i <= 10; i++) {
     console.log("[" + i + "]");
     calculadora(i, 10);
-}
+}*/
+
+calculadora(1, 2, true);
+calculadora(2, 1);
+
