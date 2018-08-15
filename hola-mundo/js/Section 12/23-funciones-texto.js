fcdd4f6672f2a50cebdc9703d8d6e97ad0e81470
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-15 12:13:27 -0500

    Section 12 - Lecture 49

---
+++ hola-mundo/js/Section 12/23-funciones-texto.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Transformacion de cadenas de texto
+var numero = 444;
+var texto1 = "Bienvenido al curso de JavaScript.";
+var texto2 = "Es un muy buen curso.";
+/*
+var dato = numero.toString();
+console.log(dato);
+console.log(typeof(dato));
+
+dato = texto1.toUpperCase();
+console.log(dato);
+console.log(dato.toLowerCase());
+
+// Calcular longitud
+
+var nombre = "Alberto Alan Zul Rabasa";
+console.log(nombre.length);
+*/
+// Concatenar - Unir textos
+
+var textoTotal = texto1.concat(" " + texto1);
+console.log(textoTotal);
