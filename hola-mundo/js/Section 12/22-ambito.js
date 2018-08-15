88f5cc7437fd6751081857c68ce15cad5f4ac682
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-15 12:04:21 -0500

    Section 12 - Lecture 48

---
+++ hola-mundo/js/Section 12/22-ambito.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function holaMundo(texto) {
+    var holaMundoTexto = "Texto dentro de la funcion.";
+    console.log(texto);
+    console.log(typeof(numero.toString()));
+    console.log(holaMundoTexto);
+}
+
+var numero = 11;
+var texto = "Hola mundo soy una variable global";
+
+holaMundo(texto);
