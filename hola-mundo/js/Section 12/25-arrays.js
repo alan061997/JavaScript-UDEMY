cf2c04c4eb637a33e2f291b821039d9b533dc47d
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-16 09:46:34 -0500

    Section 12 - Lecture 53

---
+++ hola-mundo/js/Section 12/25-arrays.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Arreglos
+
+var nombre = "Alberto";
+var nombres = ["Alberto Alan", "Luis Erick", "Rodolfo", "Arturo", 420, true];
+var lenguajes = new Array("PHP", "JS", "Java", "Go", "Python");
+
+console.log(nombres);
+console.log(lenguajes);
+
+console.log(nombres[4]);
+console.log(lenguajes[0]);
