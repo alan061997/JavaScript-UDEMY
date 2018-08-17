cbc62becb6caa71d4b317480802d8c1ce0918b00
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-08-17 11:14:32 -0500

    Section 13 - Lecture 59

--- hola-mundo/js/Section 13/26-arrays-multi.js
+++ hola-mundo/js/Section 13/26-arrays-multi.js
@@ -1,8 +1,7 @@
 "use strict";
 
 var categorias = ["Accion", "Suspenso", "Comedia", "Terror", "Romance", "Animado"];
-var peliculas = ["Avengers", "Hombre al agua", "Shrek", "Mama Mia!", "El exorcista", "La La Land"];
-
+var peliculas = ["Hombre al agua", "Shrek", "Mama Mia!", "El exorcista", "La La Land", "Avengers"];
 var cine = [categorias, peliculas];
 
 /*
@@ -12,21 +11,28 @@ console.log(cine[1]);
 console.log(cine[0][1]);
 */
 
-/*do{
+/*
+do{
     var elemento = prompt("Introduce una pelicula: ", "Shrek 3");
     peliculas.push(elemento);
 } while(elemento != "ACABAR");
 
 peliculas.pop();
-console.log(peliculas);*/
+console.log(peliculas);
+*/
 
+/*
 var indice = peliculas.indexOf("El exorcista");
 
 if (indice > -1) {
     peliculas.splice(indice, 1);
 }
+*/
+peliculas.sort();
+peliculas.reverse();
 var peliculas_string = peliculas.join();
-var cadena = peliculas_string.split(",")
+
+var cadena = peliculas_string.split(",");
 console.log(cadena);
 console.log(peliculas_string);
 
